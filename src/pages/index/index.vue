eb194b319812c07369458bced41cf1f014c380ea
tigerk <kimhwawoon@gmail.com>
2026-01-27 22:11:41 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="index">
+    <nut-button type="primary" @click="onClick">按钮</nut-button>
+  </view>
+</template>
+
+<script setup>
+import Taro from '@tarojs/taro'
+const onClick = () => {
+  Taro.showToast({
+    title: '你成功了',
+    icon: 'none',
+    duration: 2000
+  })
+}
+</script>
